d7a8e18cb5a75db52c1adeb8a7414c538a03dffd
Felipe Mendonca <felipemom2001@gmail.com>
2022-10-07 01:08:46 +0200

    chore: change jest config

--- src/presentation/components/LoginHeader/index.tsx
+++ src/presentation/components/LoginHeader/index.tsx
@@ -1,7 +1,14 @@
 import Styles from './styles.scss';
 
 const LoginHeader: React.FC = () => {
-  return <header></header>;
+  return (
+    <header className={Styles.loginHeader}>
+      <section className={Styles.innerContainer}>
+        <span className={Styles.logo}>SocialCards</span>
+      </section>
+      <div className={Styles.downLine}></div>
+    </header>
+  );
 };
 
 export default LoginHeader;
