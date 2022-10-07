d2c599ac2fb14d0d56003204c4216f5ba7b14fb6
Felipe Mendonca <felipemom2001@gmail.com>
2022-10-07 06:39:03 +0200

    refactor: move LocalValidator to its own file

---
+++ src/infra/adapters/validator/localValidator.ts
@@ -0,0 +1,14 @@
+import { Validator } from '@/data/protocols';
+
+export class LocalValidator implements Validator {
+  emailIsValid(email: string): boolean {
+    const emailRegexp = /^(\w+[/./-]?){1,}@[a-z]+[/.]\w{2,}$/;
+    return emailRegexp.test(email);
+  }
+
+  passwordIsValid(password: string): boolean {
+    const passwordRegexp = /^(?=.*[A-Z])(?=.*[0-9])(?=.*[a-z]).{6,12}$/;
+
+    return passwordRegexp.test(password);
+  }
+}
--- src/infra/adapters/validator/validatorAdapter.test.ts
+++ src/infra/adapters/validator/validatorAdapter.test.ts
@@ -1,22 +1,8 @@
-import { Validator } from '@/data/protocols';
 import { faker } from '@faker-js/faker';
 
-const makeSut = (): Validator => {
-  class LocalValidator implements Validator {
-    emailIsValid(email: string): boolean {
-      const emailRegexp = /^(\w+[/./-]?){1,}@[a-z]+[/.]\w{2,}$/;
-      return emailRegexp.test(email);
-    }
+import { LocalValidator } from './localValidator';
 
-    passwordIsValid(password: string): boolean {
-      const passwordRegexp = /^(?=.*[A-Z])(?=.*[0-9])(?=.*[a-z]).{6,12}$/;
-
-      return passwordRegexp.test(password);
-    }
-  }
-
-  return new LocalValidator();
-};
+const makeSut = () => new LocalValidator();
 
 describe('Validator adapter', () => {
   it('Should return false if an invalid email is provided', () => {
