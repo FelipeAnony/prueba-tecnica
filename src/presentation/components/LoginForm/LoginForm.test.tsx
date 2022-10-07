babe3576469a41f1fd0ea184e935076f58e98ebd
Felipe Mendonca <felipemom2001@gmail.com>
2022-10-07 20:25:28 +0200

    test: add test to LoginForm component

---
+++ src/presentation/components/LoginForm/LoginForm.test.tsx
@@ -0,0 +1,77 @@
+/**
+ * @jest-environment jsdom
+ */
+
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+
+import LoginForm from '.';
+
+import { mockUseLoginForm } from '@/presentation/mocks';
+
+jest.mock('./useLoginForm');
+
+const renderSut = () => render(<LoginForm />);
+
+describe('Login Form component', () => {
+  it('Should render appropiately without any error', () => {
+    mockUseLoginForm({});
+    renderSut();
+
+    expect(screen.getByRole('heading', { name: 'Login' })).toBeInTheDocument();
+    expect(screen.getByRole('textbox', { name: 'Email' })).toBeInTheDocument();
+    expect(screen.getByLabelText('Password')).toBeInTheDocument();
+    expect(screen.getByRole('button', { name: 'Login' })).toBeInTheDocument();
+  });
+
+  it('Should calls handleChange method when user types', async () => {
+    const { handleChange } = mockUseLoginForm({});
+    renderSut();
+
+    await userEvent.type(screen.getByRole('textbox', { name: /email/i }), 'a');
+    await userEvent.type(screen.getByLabelText(/password/i), 'a');
+
+    expect(handleChange).toHaveBeenCalledTimes(2);
+  });
+
+  it('Should not render error at start', () => {
+    mockUseLoginForm({});
+    renderSut();
+
+    expect(screen.queryByText(/error/i)).not.toBeInTheDocument();
+  });
+
+  it('Should render error when an error exists', () => {
+    mockUseLoginForm({ error: new Error('error') });
+    renderSut();
+
+    expect(screen.getByText(/error/i)).toBeInTheDocument();
+  });
+
+  it('Should disable login button if buttonIsDisabled state is true', () => {
+    mockUseLoginForm({});
+    renderSut();
+
+    expect(screen.getByRole('button')).toHaveAttribute('disabled');
+  });
+
+  it('Should enable login button if buttonIsDisabled state is false', () => {
+    mockUseLoginForm({ buttonIsDisabled: false });
+    renderSut();
+
+    expect(screen.getByRole('button')).not.toHaveAttribute('disabled');
+  });
+
+  it('Should calls handleSubmit method when user clicks Login button', async () => {
+    const loginData = { email: 'valid@mail.com', password: '1234aA' };
+    const { handleSubmit } = mockUseLoginForm({
+      loginData,
+      buttonIsDisabled: false,
+    });
+    renderSut();
+
+    await userEvent.click(screen.getByRole('button'));
+
+    expect(handleSubmit).toBeCalled();
+  });
+});
---
+++ src/presentation/mocks/mockUseLoginForm.ts
@@ -0,0 +1,36 @@
+import { useLoginForm } from '../components/LoginForm/useLoginForm';
+
+const mockedUseLoginForm = useLoginForm as jest.MockedFn<typeof useLoginForm>;
+
+type Params = {
+  loginData?: { email: string; password: string };
+  buttonIsDisabled?: boolean;
+  error?: Error | null;
+};
+
+const loginDataDefault = { email: '', password: '' };
+
+export const mockUseLoginForm = ({
+  loginData = loginDataDefault,
+  buttonIsDisabled = true,
+  error = null,
+}: Params) => {
+  const handleChange = jest.fn(() => {});
+  const handleSubmit = jest.fn(async () => {});
+
+  mockedUseLoginForm.mockReturnValueOnce({
+    loginData,
+    buttonIsDisabled,
+    error,
+    handleChange,
+    handleSubmit,
+  });
+
+  return {
+    handleChange,
+    handleSubmit,
+    loginData,
+    buttonIsDisabled,
+    error,
+  };
+};
