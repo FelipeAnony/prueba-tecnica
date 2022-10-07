97e7110cdc4021ef098d0bd593e0a43f026adec7
Felipe Mendonca <felipemom2001@gmail.com>
2022-10-07 05:17:12 +0200

    docs: add coments to authentication usecase

--- src/domain/usecases/authentication.ts
+++ src/domain/usecases/authentication.ts
@@ -6,5 +6,7 @@ export type AuthParams = {
 };
 
 export interface Authentication {
+  // Retorna una promise por que en teoria una autenticacion deveria ser online.
+  // De este modo se queda mas parecido a como seria echo en un caso real
   auth(params: AuthParams): Promise<AccountModel>;
 }
