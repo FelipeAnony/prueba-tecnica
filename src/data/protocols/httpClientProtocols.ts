5c52fc0e39e464d6e7b60cc9a14589b930c5cb9b
Felipe Mendonca <felipemom2001@gmail.com>
2022-10-08 17:51:57 +0200

    feat: create httpClient wrapper protocol

--- src/data/protocols/httpClientProtocols.ts
+++ src/data/protocols/httpClientProtocols.ts
@@ -3,3 +3,5 @@ import { HttpResponse } from '@/domain/protocols';
 export interface HttpGetClient {
   get(url: string): Promise<HttpResponse>;
 }
+
+export interface HttpClient extends HttpGetClient {}
