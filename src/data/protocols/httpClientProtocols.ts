0854d507b105c3e68c3dd192e8c233c17d5bfcdd
Felipe Mendonca <felipemom2001@gmail.com>
2022-10-09 07:04:55 +0200

    docs: update docs

--- src/data/protocols/httpClientProtocols.ts
+++ src/data/protocols/httpClientProtocols.ts
@@ -1,5 +1,11 @@
 import { HttpResponse } from '@/domain/protocols';
 
+// Para cada método, deberíamos tener una interface para respetar el ISP -
+// Interface Segregation Principle, y luego los juntamos en el wrapper que
+// será utilizado mas adelante, para implementar el httpClient en infra layer. Hice unicamente
+// el método get, por qué los demás métodos no serán utilizados. Haciéndolo de esta manera,
+// se nos facilita mucho en el momento de testear.
+
 export interface HttpGetClient {
   get(url: string): Promise<HttpResponse>;
 }
