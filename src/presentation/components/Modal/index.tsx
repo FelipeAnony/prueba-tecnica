a050082fdd591255be803cbe4f1214d1923bd56c
Felipe Mendonca <felipemom2001@gmail.com>
2022-10-09 16:49:53 +0200

    feat: create Modal component

---
+++ src/presentation/components/Modal/index.tsx
@@ -0,0 +1,27 @@
+import Button from '../Button';
+import Styles from './styles.scss';
+
+type Props = {
+  closeModalFn: () => void;
+  children: JSX.Element;
+};
+
+const Modal: React.FC<Props> = ({ closeModalFn, children }) => {
+  return (
+    <div onClick={closeModalFn} className={Styles.modalWindow}>
+      <section
+        onClick={(e) => e.stopPropagation()}
+        className={Styles.modalContainer}
+      >
+        <div className={Styles.buttonContainer}>
+          <Button style="clear" onClick={closeModalFn}>
+            X
+          </Button>
+        </div>
+        {children}
+      </section>
+    </div>
+  );
+};
+
+export default Modal;
