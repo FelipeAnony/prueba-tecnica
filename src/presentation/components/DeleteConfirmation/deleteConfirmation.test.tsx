f5208b9f51c4db9cbc7a4c1cd7e49f218fe538ad
Felipe Mendonca <felipemom2001@gmail.com>
2022-10-11 04:56:52 +0200

    tests: add tests to DeleteConfirmation

---
+++ src/presentation/components/DeleteConfirmation/deleteConfirmation.test.tsx
@@ -0,0 +1,47 @@
+/**
+ * @jest-environment jsdom
+ */
+
+import { screen, render } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+
+import DeleteConfirmation from '.';
+
+const deleteFn = jest.fn(() => {});
+const cancelDeleteFn = jest.fn(() => {});
+
+const renderSut = () => {
+  render(
+    <DeleteConfirmation
+      deleteFn={deleteFn}
+      cancelDeleteFn={cancelDeleteFn}
+      name="any-name"
+    />
+  );
+};
+
+describe('DeleteConfirmation component', () => {
+  it('Should render appropiately without any error', () => {
+    renderSut();
+
+    expect(screen.getByText(/any-name/)).toBeInTheDocument();
+    expect(
+      screen.getByRole('button', { name: /no, cancel/i })
+    ).toBeInTheDocument();
+    expect(
+      screen.getByRole('button', { name: /yes, delete/i })
+    ).toBeInTheDocument();
+  });
+
+  it('Should call cancelDeleteFn when user clicks cancel button', async () => {
+    renderSut();
+    await userEvent.click(screen.getByRole('button', { name: /no, cancel/i }));
+    expect(cancelDeleteFn).toBeCalled();
+  });
+
+  it('Should call deleteFn when user clicks delete button', async () => {
+    renderSut();
+    await userEvent.click(screen.getByRole('button', { name: /yes, delete/i }));
+    expect(deleteFn).toBeCalled();
+  });
+});
