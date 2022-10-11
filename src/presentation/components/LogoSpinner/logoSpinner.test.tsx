0cfebcfeee5a9bea23bc512fb7fbbf8b5fe2a68c
Felipe Mendonca <felipemom2001@gmail.com>
2022-10-11 06:16:21 +0200

    tests: add tests to LogoSpinner

---
+++ src/presentation/components/LogoSpinner/logoSpinner.test.tsx
@@ -0,0 +1,16 @@
+/**
+ * @jest-environment jsdom
+ */
+
+import { render, screen } from '@testing-library/react';
+import LogoSpinner from '.';
+
+const renderSut = () => render(<LogoSpinner />);
+
+describe('Login Header component', () => {
+  it('Should render appropiately without any error', () => {
+    renderSut();
+    expect(screen.getByText(/socialcards/i)).toBeInTheDocument();
+    expect(screen.getByText(/loading.../i)).toBeInTheDocument();
+  });
+});
