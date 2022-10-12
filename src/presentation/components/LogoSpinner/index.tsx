b23b32c35d1d38559a544f8556ce17a3bd9fc152
Felipe Mendonca <felipemom2001@gmail.com>
2022-10-12 02:09:17 +0200

    feat: add translations to LogoSpinner

--- src/presentation/components/LogoSpinner/index.tsx
+++ src/presentation/components/LogoSpinner/index.tsx
@@ -1,11 +1,15 @@
+import { useTranslation } from 'react-i18next';
+
 import Styles from './styles.scss';
 
 const LogoSpinner: React.FC = () => {
+  const { t } = useTranslation();
+
   return (
     <section className={Styles.logoSpinner}>
       <div className={Styles.logoLetters}>SocialCards</div>
       <div className={Styles.spinner}></div>
-      <span className={Styles.loading}>Loading...</span>
+      <span className={Styles.loading}>{t('loading')}...</span>
     </section>
   );
 };
