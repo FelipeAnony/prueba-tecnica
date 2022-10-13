c30d2198a1f98661cd3b97bfaf224dd1e1c26a19
Felipe Mendonca <felipemom2001@gmail.com>
2022-10-13 03:34:41 +0200

    chore: update jest config to better coverage report

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,16 @@
 module.exports = {
   roots: ['<rootDir>/src'],
-  collectCoverageFrom: ['<rootDir>/src/**/*.{ts, tsx}', '!**/*.d.ts'],
+  collectCoverageFrom: ['<rootDir>/src/**/*.ts', '<rootDir>/src/**/*.tsx'],
+  coveragePathIgnorePatterns: [
+    '/node_modules/',
+    '<rootDir>/src/domain',
+    '<rootDir>/src/data/protocols',
+    '<rootDir>/src/main',
+    '<rootDir>/src/presentation/router',
+    '<rootDir>/src/presentation/hooks',
+    '<rootDir>/src/presentation/pages',
+    '<rootDir>/src/store',
+  ],
   coverageDirectory: 'coverage',
   testEnvironment: 'node',
   transform: {
